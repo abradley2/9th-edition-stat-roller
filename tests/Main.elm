a82865031959a77729a7eee34286b99e991cd6f0
Tony <antbradley91@gmail.com>
2021-03-26 07:02:45 -0400

    add test module

---
+++ tests/Main.elm
@@ -0,0 +1,11 @@
+module Main exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "The main module"
+        <| pass "Yay"
